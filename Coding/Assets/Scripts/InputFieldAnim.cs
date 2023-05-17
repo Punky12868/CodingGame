c07d16a0eb6397497239d979fdc4d58c370e174f
Punky12868 <diegob12868@gmail.com>
2023-05-17 02:23:40 -0400

    fix

--- Coding/Assets/Scripts/InputFieldAnim.cs
+++ Coding/Assets/Scripts/InputFieldAnim.cs
@@ -6,23 +6,53 @@ using TMPro;
 
 public class InputFieldAnim : MonoBehaviour
 {
-    [SerializeField] RectTransform panel;
+    GameObject startButtonGO;
+    GameObject restartButtonGO;
+    GameObject inputFieldGO;
     [SerializeField] TextMeshProUGUI startButton;
     [SerializeField] TextMeshProUGUI restartButton;
+    [SerializeField] RectTransform panel;
     [SerializeField] TextMeshProUGUI textInputPlaceHolder;
     Vector2 open;
     Vector2 closed;
+    Vector3 openInputPos;
+    Vector3 closeInputPos;
+    Vector3 openStartButtonPos;
+    Vector3 closeStartButtonPos;
+    Vector3 openRestartButtonPos;
+    Vector3 closeRestartButtonPos;
     Color openColor;
     Color closedColor;
     bool state;
-    public float speed;
+    [SerializeField] float yButtonPos;
+    [SerializeField] float yInputPos;
+    [SerializeField] float speed;
+    [SerializeField] float speedSecond;
     private void Awake()
     {
+        startButtonGO = GameObject.FindGameObjectWithTag("StartB");
+        restartButtonGO = GameObject.FindGameObjectWithTag("RestartB");
+        inputFieldGO = GameObject.FindGameObjectWithTag("InputField");
+
         open = new Vector2(panel.sizeDelta.x, 932.99f);
         closed = new Vector2(panel.sizeDelta.x, 40);
-        closedColor = new Color(startButton.color.r, startButton.color.g, startButton.color.b, 0);
+
+        openInputPos = new Vector3(inputFieldGO.transform.position.x, inputFieldGO.transform.position.y);
+        closeInputPos = new Vector3(inputFieldGO.transform.position.x, inputFieldGO.transform.position.y - yInputPos);
+
+        openStartButtonPos = new Vector3(startButtonGO.transform.position.x, startButtonGO.transform.position.y);
+        closeStartButtonPos = new Vector3(startButtonGO.transform.position.x, startButtonGO.transform.position.y - yButtonPos);
+
+        openRestartButtonPos = new Vector3(restartButtonGO.transform.position.x, restartButtonGO.transform.position.y);
+        closeRestartButtonPos = new Vector3(restartButtonGO.transform.position.x, restartButtonGO.transform.position.y - yButtonPos);
+
         openColor = new Color(startButton.color.r, startButton.color.g, startButton.color.b, startButton.color.a);
+        closedColor = new Color(startButton.color.r, startButton.color.g, startButton.color.b, 0);
+
         panel.sizeDelta = closed;
+        inputFieldGO.transform.position = closeInputPos;
+        startButtonGO.transform.position = closeStartButtonPos;
+        restartButtonGO.transform.position = closeRestartButtonPos;
     }
     private void Update()
     {
@@ -30,9 +60,15 @@ public class InputFieldAnim : MonoBehaviour
         if (state)
         {
             panel.sizeDelta = Vector3.Lerp(panel.sizeDelta, open, speed * Time.deltaTime);
-            if (panel.sizeDelta.y > 932.90)
+            inputFieldGO.transform.position = Vector3.Lerp(inputFieldGO.transform.position, openInputPos, speedSecond * Time.deltaTime);
+            startButtonGO.transform.position = Vector3.Lerp(startButtonGO.transform.position, openStartButtonPos, speedSecond * Time.deltaTime);
+            restartButtonGO.transform.position = Vector3.Lerp(restartButtonGO.transform.position, openRestartButtonPos, speedSecond * Time.deltaTime);
+            if (panel.sizeDelta.y > 932.985)
             {
                 panel.sizeDelta = open;
+                inputFieldGO.transform.position = openInputPos;
+                startButtonGO.transform.position = openStartButtonPos;
+                restartButtonGO.transform.position = openRestartButtonPos;
             }
             else if (panel.sizeDelta.y > 897)
             {
@@ -48,9 +84,15 @@ public class InputFieldAnim : MonoBehaviour
         else
         {
             panel.sizeDelta = Vector3.Lerp(panel.sizeDelta, closed, speed * Time.deltaTime);
+            inputFieldGO.transform.position = Vector3.Lerp(inputFieldGO.transform.position, closeInputPos, speedSecond * Time.deltaTime);
+            startButtonGO.transform.position = Vector3.Lerp(startButtonGO.transform.position, closeStartButtonPos, speedSecond * Time.deltaTime);
+            restartButtonGO.transform.position = Vector3.Lerp(restartButtonGO.transform.position, closeRestartButtonPos, speedSecond * Time.deltaTime);
             if (panel.sizeDelta.y < 39.99f)
             {
                 panel.sizeDelta = closed;
+                inputFieldGO.transform.position = closeInputPos;
+                startButtonGO.transform.position = closeStartButtonPos;
+                restartButtonGO.transform.position = closeRestartButtonPos;
             }
             else if (panel.sizeDelta.y < 899.05)
             {
